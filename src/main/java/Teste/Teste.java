467016365fc0f97ac88ad63dcdd51bc667b2549c
raf <raf@DESKTOP-PN9HH5P>
2022-04-10 12:38:50 -0300

    classe para teste de funções

---
+++ src/main/java/Teste/Teste.java
@@ -0,0 +1,10 @@
+package Teste;
+import java.util.Scanner;
+/** 
+   por Rafael Ferreira Goulart
+**/
+public class Teste {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+    }
+}
