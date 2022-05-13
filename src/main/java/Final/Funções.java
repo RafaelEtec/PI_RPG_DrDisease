8055e1fd13b7d53b2a75b982e979c8d7d8be0e22
raf <raf@DESKTOP-PN9HH5P>
2022-05-13 16:08:42 -0300

    Iniciado

---
+++ src/main/java/Final/Funções.java
@@ -0,0 +1,13 @@
+package Final;
+import java.util.Scanner;
+/** 
+   por Rafael Ferreira Goulart
+**/
+public class Funções {
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
